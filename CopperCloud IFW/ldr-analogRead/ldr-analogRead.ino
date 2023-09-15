70098a38e4b456f883697c570d7cc97a8272e730
CopperCloud IOTech <45527883+coppercloud-iotech@users.noreply.github.com>
2023-09-15 13:28:52 +0530

    Update ldr-analogRead.ino

--- CopperCloud IFW/ldr-analogRead/ldr-analogRead.ino
+++ CopperCloud IFW/ldr-analogRead/ldr-analogRead.ino
@@ -2,12 +2,14 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(A0, INPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   long data = analogRead(A0);   // turn the LED on (HIGH is the voltage level)
   Serial.println(data);
+  int percent = map(data, 0, 1023, 0, 100);
+  Serial.print(percent); Serial.println("%");
   delay(1000);                       // wait for a second
 }
